8fa87621f49a8fa2fea010af3eeb73e66bdd0915
JeffieJansson <153088580+JeffieJansson@users.noreply.github.com>
2023-12-19 17:04:11 +0100

    loopisar.js

--- loopisar.js
+++ loopisar.js
@@ -16,69 +16,77 @@ const localTime = now.toLocaleString();
 
 // Update the data layer with the current local time
 dataLayer.forEach(item => {
-item.time = localTime;
+  item.time = localTime;
 });
 
-// Task 1: Loop through the existing data
-dataLayer.forEach(event => {
-console.log(`Event: ${event.event}, Time: ${event.time}`);
-});
-
-// Task 2: If new data, add it to the appropriate list
-function logEvent(event, details) {
-dataLayer.push({ event: event, time: localTime, details: details });
-console.log(`Event: ${event}`);
+// Loop through existing data
+function loopThroughData() {
+  dataLayer.forEach(event => {
+    console.log(`Event: ${event.event}, Time: ${event.time}`);
+  });
 }
 
-// Task 3: If existing data, modify the data layer
+// Add new data to the appropriate list
+function logEvent(event, details) {
+  dataLayer.push({ event: event, time: localTime, details: details });
+  console.log(`Event: ${event}`);
+}
+
+// Modify existing data in the data layer
 function modifyData(event, newProperty) {
-dataLayer.forEach(item => {
-  if (item.event === event) {
-    item.newProperty = newProperty;
-  }
-});
+  dataLayer.forEach(item => {
+    if (item.event === event) {
+      item.newProperty = newProperty;
+    }
+  });
+}
+
+// Filter which data to collect
+function filterDataToCollect() {
+  document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', function() {
+      console.log(`Click on button: ${this.textContent}`);
+      // Customize what you want to track for each button
+    });
+  });
+
+  document.querySelectorAll('input').forEach(input => {
+    input.addEventListener('focus', function() {
+      console.log(`Focused on input: ${this.name}`);
+      // Customize what you want to track for each input focus
+    });
+  });
 }
 
 // Event listeners for buttons and form
 document.getElementById('buttonA').addEventListener('click', function() {
-logEvent('Click on Button A', null);
+  logEvent('Click on Button A', null);
 });
 
 document.getElementById('buttonB').addEventListener('click', function() {
-logEvent('Click on Button B', null);
+  logEvent('Click on Button B', null);
 });
 
 document.getElementById('button1').addEventListener('click', function() {
-logEvent('Click on Button 1', null);
+  logEvent('Click on Button 1', null);
 });
 
 document.getElementById('button2').addEventListener('click', function() {
-logEvent('Click on Button 2', null);
+  logEvent('Click on Button 2', null);
 });
 
 document.getElementById('button3').addEventListener('click', function() {
-logEvent('Click on Button 3', null);
+  logEvent('Click on Button 3', null);
 });
 
 document.getElementById('trackedForm').addEventListener('submit', function(event) {
-event.preventDefault();
-const formData = new FormData(event.target);
-logEvent('Form Submission', Object.fromEntries(formData.entries()));
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  logEvent('Form Submission', Object.fromEntries(formData.entries()));
 });
 
-// Task 4: Filter which data to collect
-// Here, events are logged; you can customize it according to your specific data collection needs
-// For example, filter to collect specific events or types of events
-document.querySelectorAll('button').forEach(button => {
-button.addEventListener('click', function() {
-  console.log(`Click on button: ${this.textContent}`);
-  // Customize what you want to track for each button
-});
-});
+// Task 1: Loop through the existing data
+loopThroughData();
 
-document.querySelectorAll('input').forEach(input => {
-input.addEventListener('focus', function() {
-  console.log(`Focused on input: ${this.name}`);
-  // Customize what you want to track for each input focus
-});
-});
+// Task 2: Filter which data to collect
+filterDataToCollect();
